45f8d86cf74b473ded1b597aa9b217998bd681de
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-11-10 13:21:10 +0100

    add fix

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 import path from 'node:path';
 //  Встановлюємо абсолютний шлях до файлу db.json
-export const PATH_DB = path.resolve('src', 'db', 'db.json');
+export const PATH_DB = path.resolve(process.cwd(), 'src', 'db', 'db.json');
